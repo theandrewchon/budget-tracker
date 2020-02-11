dc2b056ac64c82fa85bb61b48fc9d488c34eb449
Andrew Chon <chonclan@gmail.com>
2020-02-10 21:20:23 -0600

    service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,21 +1,12 @@
 const FILES_TO_CACHE = [
   '/',
   '/index.html',
-  '/assets/css/style.css',
-  '/assets/js/loadPosts.js',
-  '/assets/images/Angular-icon.png',
-  '/assets/images/React-icon.png',
-  '/assets/images/Vue.js-icon.png',
   '/manifest.webmanifest',
-  '/favicon.ico',
-  '/assets/images/icons/icon-72x72.png',
-  '/assets/images/icons/icon-96x96.png',
-  '/assets/images/icons/icon-128x128.png',
-  '/assets/images/icons/icon-144x144.png',
-  '/assets/images/icons/icon-152x152.png',
-  '/assets/images/icons/icon-192x192.png',
-  '/assets/images/icons/icon-384x384.png',
-  '/assets/images/icons/icon-512x512.png'
+  '/public/images/icons/icon-72x72.png',
+  '/public/images/icons/icon-96x96.png',
+  '/public/images/icons//icon-144x144.png',
+  '/public/images/icons/icon-72x72.png',
+  '/public/images/icons/icon-72x72.png'
 ];
 
 const CACHE_NAME = 'static-cache-v2';
